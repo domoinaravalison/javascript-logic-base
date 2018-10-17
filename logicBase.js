f7e7428291626110a8400993165c725cac704581
ravalison <domoina.ravalison@stackerine.com>
2018-10-17 11:30:24 +0200

    exo

--- logicBase.js
+++ logicBase.js
@@ -5,3 +5,15 @@ console.log(age);
 
 !isAdult && alert('Bonjour simple user ');
 isAdult && alert('vous etes majeur');
+
+const role = prompt('quel est votre role dans la societe');
+
+const isAdmin = role === 'admin';
+
+isAdult && isAdmin && alert('Bienvenue admin');
+
+const pass = prompt('votre mot de passe');
+
+const isMaster = pass.length >= 5;
+
+isMaster && isAdmin && isAdult && alert('bienvenu master user');
